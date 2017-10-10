320f9821e559d4e758c26d530c50e1a76f0e32ff
mliu <mliu@iastate.edu>
2017-10-10 12:06:26 -0500

    Connected and configured MongoDB and updated all temperature features accordingly

--- Server.py
+++ Server.py
@@ -1,15 +1,25 @@
 from flask import Flask, request
+from flask_pymongo import PyMongo
+
+# Configurations
 app = Flask(__name__)
+mongo = PyMongo(app)
+
+# Contants
+CONST_MINIMUM_TEMP_TYPE = "Minimum";
+CONST_MAXIMUM_TEMP_TYPE = "Maximum";
+CONST_SET_TEMP_TYPE = "Set";
+
 
 # Route for GET and POST to our set temperature.
 @app.route('/set-temperature', methods=['GET', 'POST'])
 def set_temperature():
     if request.method == 'POST':
         temperature = request.form['temperature'];
-        # Write to our persistence what the set temperature it
+        update_temperature(CONST_SET_TEMP_TYPE, temperature);
         return str(temperature);
     else:
-        temperature = get_set_temperature();
+        temperature = get_temperature(CONST_SET_TEMP_TYPE);
         return str(temperature);
     #endif
 #enddef
@@ -18,11 +28,12 @@ def set_temperature():
 @app.route('/min-temperature', methods=['GET', 'POST'])
 def temperature_min():
     if request.method == 'POST':
+
         min_temperature = request.form['min-temperature'];
-        # Write to our persistence what the set temperature it
+        update_temperature(CONST_MINIMUM_TEMP_TYPE, min_temperature);
         return str(min_temperature);
     else:
-        min_temperature = get_min_temperature();
+        min_temperature = get_temperature(CONST_MINIMUM_TEMP_TYPE);
         return str(min_temperature);
     #endif
 #enddef
@@ -32,27 +43,27 @@ def temperature_min():
 def temperature_max():
     if request.method == 'POST':
         max_temperature = request.form['max-temperature'];
-        # Write to our persistence what the set temperature it
+        update_temperature(CONST_MAXIMUM_TEMP_TYPE, max_temperature);
         return str(max_temperature);
     else:
-        max_temperature = get_max_temperature();
+        max_temperature = get_temperature(CONST_MAXIMUM_TEMP_TYPE)
         return str(max_temperature);
     #endif
 #enddef
 
 ############################## --- Helper Functions --- ###################################
 
-# Get set temperature for the machine.
-def get_set_temperature():
-    return 200;
+# UPDATE temperature in Temperature Table of the according type.
+def update_temperature(type, temperature):
+    mongo.Temperature.update_one({"type": type}, {
+        "$set": {
+            "temperature": temperature
+        }
+    });
 #enddef
 
-# Get the minimum temperature for the machine.
-def get_min_temperature():
-    return 180;
-#enddef
-
-# Get the maximum temperature for the machine.
-def get_max_temperature():
-    return 300;
+# GET temperature in the Temperature Table for the type provided.
+def get_temperature(type):
+    temperature_object = mongo.Temperature.findOne({"type": type});
+    return temperature_object["temperature"];
 #enddef
